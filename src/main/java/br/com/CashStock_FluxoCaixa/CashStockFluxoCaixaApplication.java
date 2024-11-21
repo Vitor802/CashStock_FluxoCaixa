7acc0bf8b85169d26abf39e310d43a1259d31dbf
Shotsu1 <802.441@alunos.unigran.br>
2024-11-21 02:26:47 -0400

    Initial commit

---
+++ src/main/java/br/com/CashStock_FluxoCaixa/CashStockFluxoCaixaApplication.java
@@ -0,0 +1,13 @@
+package br.com.CashStock_FluxoCaixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashStockFluxoCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashStockFluxoCaixaApplication.class, args);
+	}
+
+}
